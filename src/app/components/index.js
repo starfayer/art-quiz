70bb9bd5775ce60a3a2f073dc8b0c983c2bb632d
starfayer <nikita.mikhailikov@mail.ru>
2021-12-02 17:51:12 +0300

    add quiz layout

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -4,8 +4,9 @@ import footer from "./Footer";
 import button from "./Button"
 import menu from "./Menu"
 import header from "./Header"
+import quiz from "./Quiz"
 
 let components = {
-  button, settings, header, home, footer, menu
+  button, settings, header, home, footer, menu, quiz
 }
 export default components;
